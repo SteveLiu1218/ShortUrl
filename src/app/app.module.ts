963539abce67e48c1cf4f1728f4efff6a48a96de
CBTC\CurtisLiu <curtisliu@chipbond.com.tw>
2022-10-28 13:23:41 +0800

    1.Form Save Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShortUrlFormComponent } from './short-url-form/short-url-form.component';
 
+// PrimeNg
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AccordionModule } from 'primeng/accordion';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +19,11 @@ import { ShortUrlFormComponent } from './short-url-form/short-url-form.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    AccordionModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/short-url-form/short-url-form.component.ts
+++ src/app/short-url-form/short-url-form.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { UrlModel } from '../Model/UrlModel'; { }
+import {
+  FormBuilder,
+  NgForm,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
 
 @Component({
   selector: 'app-short-url-form',
@@ -6,10 +16,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./short-url-form.component.css']
 })
 export class ShortUrlFormComponent implements OnInit {
-
-  constructor() { }
+  formShorter!: FormGroup;
+  reg =
+    "((http|https)://)(www.)?" +
+    "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" +
+    "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)";
+  @Input() urlModel?: UrlModel;
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formShorter = this.fb.group({      //FormGroup
+      longUrl: ["", [Validators.required, Validators.pattern(this.reg)]], //FormControlName
+      shortUrl: new FormControl({ value: null, disabled: true })
+    });
   }
-
+  Save(form: FormGroup): void {
+    console.log(form.value["longUrl"]);
+  }
 }
