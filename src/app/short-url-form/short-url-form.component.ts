81e339d541df386a4be88f469c635f1b4243bb0b
CBTC\CurtisLiu <curtisliu@chipbond.com.tw>
2022-10-31 17:31:54 +0800

    Add comment

--- src/app/short-url-form/short-url-form.component.ts
+++ src/app/short-url-form/short-url-form.component.ts
@@ -10,6 +10,7 @@ import {
   Validators
 } from "@angular/forms";
 import { BitlyService } from '../Service/bitly.service';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-short-url-form',
@@ -27,20 +28,29 @@ export class ShortUrlFormComponent implements OnInit {
     public bitly: BitlyService) { }
 
   ngOnInit(): void {
+    //定義接到的form 的 Schema
+    //詳細方法 參考 https://angular.io/guide/reactive-forms , https://ithelp.ithome.com.tw/articles/10195280
     this.formShorter = this.fb.group({      //FormGroup
-      longUrl: ["", [Validators.required, Validators.pattern(this.reg)]], //FormControlName
+      //寫法1
+      //["", [Validators.required, Validators.pattern(this.reg)]], //FormControlName
+      longUrl: new FormControl({ value: null, disabled: false }, [Validators.required, Validators.pattern(this.reg)]),//寫法2
       shortUrl: new FormControl({ value: null, disabled: true })
     });
+
   }
   Save(form: FormGroup): void {
     if (form.valid) {
       //Call Bitty API
+      //Observable 在Select的時候一定是搭配subscribe
       this.bitly.shortUrl(form.value["longUrl"]).subscribe(
         (l) => {
-          this.formShorter.patchValue({ shortUrl: l.link })
+          this.formShorter.patchValue({ shortUrl: l.link }) //還有SetValue 可以用 https://ithelp.ithome.com.tw/articles/10195280
         }
       )
-
     }
+  }
+  //取form 整個的方法 //console.log(this.accountControl.value) 可驗證
+  get accountControl(): FormControl {
+    return this.formShorter.get('longUrl') as FormControl  //form.value["longUrl"] 一樣意思
   }
 }
